e7ee71d41eb03e5d71740ecb8a50bbde67aa1b52
KANASIEL <kanasieltg@gmail.com>
2025-08-28 10:11:29 +0900

    Update test0828.java

--- test0828.java
+++ test0828.java
@@ -2,19 +2,21 @@ import java.util.Random;
 
 public class test0828 {
     public static void main(String[] args) {
-        int[][] intArry = new int[3][4];
         Random random = new Random();
+        
+        int[][] intArry = new int[3][4];
         int sum = 0;
+        
         for (int i = 0; i < intArry.length; i++) {
             for (int j = 0; j < intArry[i].length; j++) {
                 intArry[i][j] = random.nextInt(10);
                 sum += intArry[i][j];
                 System.out.print(intArry[i][j] + "\t");
-
             }
             System.out.println();
         }
+        
         System.out.println("합은 "+ sum);
-
     }
 }
+
